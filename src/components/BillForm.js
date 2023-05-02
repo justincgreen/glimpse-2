d992d5708896a9ce0463e45690311358ecbda33b
Justin Green <justingreendev@gmail.com>
2023-05-01 21:14:35 -0500

    Inital set up of bill form

--- src/components/BillForm.js
+++ src/components/BillForm.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import GlobalContext from '@/context/GlobalContext';
 
 const BillForm = () => {
@@ -8,16 +8,50 @@ const BillForm = () => {
     displayBillForm,
     setDisplayBillForm
   } = useContext(GlobalContext);
-   
+  
+  const [disableSubmitBtn, setDisableSubmitBtn] = useState(false);
+  
+  // Toggle bill form 
   const toggleBillForm = () => {
     setDisplayBillForm(!displayBillForm);
   }
+  
+  // Capture form data
+  const captureFormData = () => {
+    setDisableSubmitBtn(true);
+  }
+  
+  const renderForm = () => {
+    return (
+      <form className="c-bill-form__form">
+        <label className="c-bill-form__label">Description</label>
+        <input type="text" placeholder="Enter description" className="c-bill-form__input c-bill-form__input--description" />        
+        <label className="c-bill-form__label">Amount</label>
+        <input type="number" min="0" placeholder="Enter Amount" className="c-bill-form__input c-bill-form__input--amount" />
+        <label className="c-bill-form__label">Due Date</label>
+        <input type="date" className="c-bill-form__input c-bill-form__input--date" />
+        {
+          disableSubmitBtn 
+          ?
+          <button className="button" disabled>Add Expense</button>
+          :
+          <button className="button" onClick={captureFormData}>Add Expense</button>
+        }
+      </form>
+    )
+  }
    
   return (
-    <h1>
-      Bill form component - TODO: ADD FORM
-      <button className="button button--primary" onClick={toggleBillForm}>View Bills</button>
-    </h1>
+    <>
+      <div className="c-bill-form__upper">
+        <h2 className="c-bill-form__title">Add Bill</h2>
+        <button className="button button--primary" onClick={toggleBillForm}>View Bills</button>
+      </div>
+      
+      {
+        renderForm()
+      }      
+    </>
   )
 }
 
