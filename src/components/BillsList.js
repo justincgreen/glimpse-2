f8cc67cd068c54eca04b36aa48f89f5b14865c8f
Justin Green <justingreendev@gmail.com>
2023-05-24 22:30:33 -0500

    Reverse transactions array order

--- src/components/BillsList.js
+++ src/components/BillsList.js
@@ -2,13 +2,14 @@ import { useContext } from 'react';
 import GlobalContext from '@/context/GlobalContext';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 const BillsList = () => {
   const { 
     billTransactions,
     setBillTransactions
-   } = useContext(GlobalContext);
-   
+   } = useContext(GlobalContext);              
+      
   return (
     <>
       {
@@ -16,29 +17,32 @@ const BillsList = () => {
         ?
         <div className="c-bills-list">
           {
-            billTransactions.map((bill) => {
+            [...billTransactions].reverse().map((bill) => {
               return (
                 <div className="c-bill-item" key={bill.id}>
-                  <div className="c-bill-item__timestamp">{bill.timestamp}</div>
+                  {/* <div className="c-bill-item__timestamp">{bill.timestamp}</div> */}
+                  <span className="c-bill-item__due-date">Due date: {bill.dueDate}</span>
                   <span className="c-bill-item__description">{bill.description}</span>
                   <span className="c-bill-item__amount">${bill.amount}</span>
-                  <span className="c-bill-item__due-date">Due date: {bill.dueDate}</span>
-                  <span className="c-bill-item__delete" onClick={
-                    () => {
-                      alert('deleted - hook up');
-                      //deleteExpenseModal(item.id, item.amount)
-                    }
-                  }>
-                    <DeleteForeverIcon sx={{ color: '#ff4e4e', fontSize: '28px' }} />
-                  </span>
-                  <span className="c-bill-item__edit" onClick={
-                    () => {
-                      alert('edited - hook up');
-                      //editExpense(item.id, item.description, item.amount)
-                    }
-                  }>
-                    <EditIcon sx={{ color: '#55d4da', fontSize: '28px' }} />
-                  </span>
+                  <div className="c-bill-item__icons">    
+                    <MoreHorizIcon sx={{ color: '#fff', fontSize: '28px' }} />                                
+                    <span className="c-bill-item__delete" onClick={
+                      () => {
+                        alert('deleted - hook up');
+                        //deleteExpenseModal(item.id, item.amount)
+                      }
+                    }>
+                      <DeleteForeverIcon sx={{ color: '#ff4e4e', fontSize: '28px' }} />
+                    </span>
+                    <span className="c-bill-item__edit" onClick={
+                      () => {
+                        alert('edited - hook up');
+                        //editExpense(item.id, item.description, item.amount)
+                      }
+                    }>
+                      <EditIcon sx={{ color: '#55d4da', fontSize: '28px' }} />
+                    </span>
+                  </div>
                 </div>
               )
             })
